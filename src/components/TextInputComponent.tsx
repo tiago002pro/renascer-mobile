8ba0bb2a64b43d94c6935e177a339033fc691100
tiago002pro <tiagobarbosa02@outlook.com>
2023-10-03 13:36:05 -0300

    Refazendo o input text com native-base

--- src/components/TextInputComponent.tsx
+++ src/components/TextInputComponent.tsx
@@ -1,38 +1,36 @@
-import { Box } from "native-base";
-import { StyleSheet, TextInput } from "react-native";
+import { Box, Icon, Input } from "native-base";
+import { StyleSheet } from "react-native";
 import { MaterialIcons } from 'react-native-vector-icons';
 import { THEME } from "../styles/theme";
 
 export default function TextInputComponent({ placeholder, icon }) {
   return (
     <Box mb={3}>
-      <TextInput
+      <Input
         placeholder={placeholder}
-        placeholderTextColor={"black"}
-        style={{
-          height: 50, 
-          fontSize: THEME.fontSizes.md, 
-          borderWidth: 1, 
-          borderRadius: 30,
-          paddingStart: 50,
-          fontFamily: "MontserratLight",
-          color: THEME.colors.blue[300],
-        }}
-      >
-      </TextInput>
-      <MaterialIcons name={icon} color={THEME.colors.blue[300]} style={styles.icon}/>
+        InputLeftElement = {
+          <Icon as = {
+            <MaterialIcons 
+              name={icon}
+              color={THEME.colors.blue[300]}
+              style={styles.icon}
+            />
+          }/>
+        }
+        borderRadius={30}
+        fontSize={"sm"}
+        lineHeight={"sm"}
+        height={"16"}
+        alignItems={"center"}
+      />
     </Box>
   )
 }
 
 const styles = StyleSheet.create({
   icon: {
-    fontSize: 25,
-    lineHeight: 25,
-    top: 12.5,
-    left: 15,
-    position: "absolute",
-    alignItems: "center",
-    justifyContent: "center",
+    fontSize: THEME.fontSizes.lg,
+    lineHeight: THEME.fontSizes.lg,
+    left: 10,
   }
 });
