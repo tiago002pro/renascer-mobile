c16c094e11eabebaff5588fe6ce26b377ca20136
tiago002pro <tiagobarbosa02@outlook.com>
2023-10-06 07:19:13 -0300

    Cadastro de novo usuário

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,15 +1,50 @@
 import { Button, Text, View } from "native-base";
 import TextInputComponent from "../../components/TextInputComponent";
+import { useState } from "react";
+import { doRegister } from "../../services/AuthenticationService";
 
-export default function Register() {
+export default function Register({ navigation }: any)
+{
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [passWord, setsetPassWordame] = useState('')
+
+  async function register() {
+    const result = await doRegister(name, email, passWord)
+    console.log("result", result.login);
+    if (result) {
+      navigation.replace('Login')
+    } else {
+      console.log("Erro");
+    }
+  }
+
   return(
     <View flex={1} p={"5%"} backgroundColor={"backgroud"}>
-      <TextInputComponent placeholder={"Nome e sobrenome"} icon={"person"}/>
-      <TextInputComponent placeholder={"E-mail"} icon={"mail"}/>
-      <TextInputComponent placeholder={"Data de nascimento"} icon={"cake"}/>
-      <TextInputComponent placeholder={"Senha"} icon={"lock"}/>
-      <TextInputComponent placeholder={"Confirmar senha"} icon={"lock"}/>
-      <Button 
+      <TextInputComponent
+        placeholder={"Nome e sobrenome"}
+        icon={"person"}
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInputComponent
+        placeholder={"E-mail"}
+        icon={"mail"}
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInputComponent
+        placeholder={"Senha"}
+        icon={"lock"}
+        passWordType={true}
+        value={passWord}
+        onChangeText={setsetPassWordame}
+      />
+      {/* <TextInputComponent placeholder={"Data de nascimento"} icon={"cake"}/> */}
+      {/* <TextInputComponent placeholder={"Confirmar senha"} icon={"lock"}/> */}
+
+      <Button
+        onPress={register}
         backgroundColor={"blue.300"}
         borderRadius={30}
         height={50}
--- src/services/AuthenticationService.ts
+++ src/services/AuthenticationService.ts
@@ -7,9 +7,25 @@ export async function doLogin(login: string, password: string) {
       login,
       password
     })
-    console.log(result);
     return result.data
   } catch(error) {
+    console.log(error);
+    return null
+  }
+}
+
+export async function doRegister(name: string, login: string, password: string) {
+  if(!name || !login || !password) return null
+  try {
+    var role: string = 'USER'
+    const result = await api.post('/auth/register', {
+      name,
+      login,
+      password,
+      role
+    })
+    return result.data
+  } catch (error) {
     console.log(error);
     return null
   }
