2135986f05a96ca05db293251ce56006f47c0cfa
tiago002pro <tiagobarbosa02@outlook.com>
2023-10-10 21:21:02 -0300

    Verificação de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
-import { VStack, Box, Image } from 'native-base';
+import { VStack, Box, Image, useToast } from 'native-base';
 import ButtonComponent from "../../components/ButtonComponent";
 import TextInputComponent from "../../components/TextInputComponent";
 import { doLogin } from "../../services/AuthenticationService";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import jwtDecode from "jwt-decode";
+
 export default function Login({ navigation }: any) {
   const [email, setEmail] = useState('')
   const [passWord, setPassWord] = useState('')
+  const [loading, setLoading] = useState(true)
+  const toast = useToast();
+
+  useEffect(() => {
+    async function verifyLogin() {
+      const token = await AsyncStorage.getItem('token')
+      if (token) {
+        navigation.replace('Tabs')
+      }
+      setLoading(false)
+    }
+    verifyLogin()
+  }, [])
 
   async function login() {
     const retult = await doLogin(email, passWord)
     if (retult) {
+      const { token } = retult
+      const tokenDecoded: any = jwtDecode(token)
+      AsyncStorage.setItem('token', token)
+      AsyncStorage.setItem('userId', tokenDecoded.id)
       navigation.replace('Tabs')
     } else {
-      console.log("Erro");
+      toast.show({
+        title: "Erro no login",
+        description: "O email ou senha não conferem",
+        backgroundColor: "red.500"
+      })
     }
+  }
+
+  if(loading) {
+    return null
   }
 
   return (
