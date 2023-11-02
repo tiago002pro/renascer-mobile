3de741a71691fe4d53f7ae9fd37816b7a142f380
tiago002pro <tiagobarbosa02@outlook.com>
2023-11-02 02:02:16 -0300

    Iniciando o projeto

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
