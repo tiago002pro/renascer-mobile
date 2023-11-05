164f4c5136047b5a5a5b786642b813e5bf4a4758
tiago002pro <tiagobarbosa02@outlook.com>
2023-11-05 11:23:53 -0300

    Ignorando o LOG SSRProvider até a solução do problema.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
 
@@ -7,8 +7,13 @@ import Routes from './src/routes';
 import { AuthProvider } from './src/contexts/auth';
 
 import { THEME } from './src/styles/theme';
+import { LogBox } from 'react-native';
 
 export default function registerRootComponentApp() {
+  useEffect(() => {
+    LogBox.ignoreLogs(['In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app.']);
+  }, []);
+
   return (
     <NativeBaseProvider theme={THEME}>
       <NavigationContainer>
