c82e76f97ba100807d23ef57fd43f830258b471e
tiago002pro <tiagobarbosa02@outlook.com>
2024-01-05 00:25:18 -0300

    Ajustando o input text para label outlined

---
+++ src/screens/components/InputText/InputText.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+import { THEME } from "../../../styles/theme";
+
+export const styles = StyleSheet.create({
+  input: {
+    backgroundColor: THEME.colors.backgroud,
+    fontSize: THEME.fontSizes.md,
+  },
+})
--- src/screens/components/InputText/index.tsx
+++ src/screens/components/InputText/index.tsx
@@ -1,17 +1,44 @@
-import { Input, View } from "native-base";
+import { Input, Text, View } from "native-base";
+import { TextInput } from "react-native-paper";
 
 import { THEME } from "../../../styles/theme";
+import { styles } from "./InputText";
+import MaskInput from "react-native-mask-input";
 
 interface InputProps {
+  label?: string;
+  type?: any;
   valiable?: any;
   setValiable?: (variable: any) => void;
   onChange?: (variable: any) => void;
+  mask?: any;
 }
 
-export default function InputTextComponent({ valiable, setValiable, onChange }: InputProps) {
+export default function InputTextComponent({ label, type, valiable, setValiable, onChange, mask }: InputProps) {
+
   return (
     <View>
-      <Input
+      <TextInput
+        label={label}
+        value={valiable}
+        onChangeText={setValiable}
+        mode='outlined'
+        keyboardType={type ? type : 'default'}
+        style={styles.input}
+        textColor="#FFF"
+        outlineStyle={{
+          borderColor: '#FFF',
+        }}
+        cursorColor={'#FFF'}
+        theme={{colors: { placeholder: 'white' , text: 'white', primary: 'white', onSurfaceVariant: 'white'}}}
+        render={props => 
+          <MaskInput
+            {...props}
+            mask={mask}
+          />
+        }
+      />
+      {/* <Input
         value={valiable}
         onChangeText={setValiable}
         onChange={onChange}
@@ -27,10 +54,10 @@ export default function InputTextComponent({ valiable, setValiable, onChange }: 
         _focus={{
           backgroundColor: THEME.colors.backgroudLight,
           borderColor: THEME.colors.yellow[400],
-          borderWidth: 2,
+          // borderWidth: 2,
           color: THEME.colors.white,
         }}
-      />
+      /> */}
     </View>
   );
 }
